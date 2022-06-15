670eb7b50a92934fec717006b697832ef426eb4b
Perry Asibey-Bonsu <40182399+dev-perry@users.noreply.github.com>
2022-06-15 12:55:30 -0500

    Dashboard modified, index route removed and dashboard redirect added to config

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,9 +7,8 @@ const Dashboard: NextPage = () => {
     <div className="pt-12 px-12 h-screen">
       <div className="flex flex-row">
       <p className="text-3xl font-default font-semibold">{today}</p>
-      <button className="h-8 ml-9 text-base rounded border px-4 border-[#427A5B] text-[#427A5B]">Create Portal</button>
       </div>
-      <div className="grid grid-cols-3 mt-8">
+      <div className="flex flex-row space-x-6 mt-6">
         <DataCard/>
         <DataCard/>
         <DataCard/>
