477675153343fb49723f76d1cb7a4798ca432525
Perry Asibey-Bonsu <40182399+dev-perry@users.noreply.github.com>
2022-06-28 17:35:03 -0500

    Reactspring installed, pre-cursor to dynamic rendering added

--- pages/portals/[id].tsx
+++ pages/portals/[id].tsx
@@ -1,14 +1,30 @@
 import FormInput from '../../components/FormInput';
+import FormInputWrapper from '../../components/FormInputWrapper';
 import PortalElementButton from '../../components/PortalElementButton';
 
 function PortalEditor(): JSX.Element {
+  const portalButtons: string[] = [
+    'text',
+    'text-area',
+    'single-choice',
+    'multi-choice',
+    'date',
+    'file',
+  ];
+  let formConfig: {type:string}[] = [
+    { type: 'text' },
+    { type: 'text-area' },
+    { type: 'single-choice' },
+    { type: 'multi-choice' },
+    { type: 'date' },
+  ];
   return (
     <div className="h-screen">
       <div className="flex flex-row h-full overflow-y-auto">
-        <div className="bg-[#F9F9F9] w-[480px] px-10 pt-8">
+        <div className="bg-[#F9F9F9] w-[480px] px-10 py-8">
           <form className="flex flex-col space-y-3 relative">
-            <FormInput type="text" label="Portal Name"/>
-            <FormInput type="text-area" label="Description"/>
+            <FormInput type="text" label="Portal Name" />
+            <FormInput type="text-area" label="Description" />
             <label className="block">
               <span className="font-medium text-base">Fee</span>
               <input
@@ -30,43 +46,63 @@ function PortalEditor(): JSX.Element {
               <FormInput type="date" label="End" />
             </div>
             <div className="grid grid-cols-3 gap-y-6 pt-8">
-            <PortalElementButton type="text"/>
-            <PortalElementButton type="text-area"/>
-            <PortalElementButton type="single-choice"/>
-            <PortalElementButton type="multi-choice"/>
-            <PortalElementButton type="date"/>
-            <PortalElementButton type="file"/>
-          </div>
-          <div className="flex flex-row space-x-5 justify-end pt-8">
-            <button className="border border-[#9D2F2F] bg-[#FFFFFF] text-[#9D2F2F] w-20 h-8 font-medium rounded">Cancel</button>
-            <button className="bg-[#427A5B] w-20 h-8 text-[#FFFFFF] font-medium rounded">Save</button>
-          </div>
+              {portalButtons.map((type) => (
+                <PortalElementButton key={type} type={type} />
+              ))}
+            </div>
+            <div className="flex flex-row space-x-5 justify-end pt-8">
+              <button className="border border-[#9D2F2F] bg-[#FFFFFF] text-[#9D2F2F] w-20 h-8 font-medium rounded">
+                Cancel
+              </button>
+              <button className="bg-[#427A5B] w-20 h-8 text-[#FFFFFF] font-medium rounded">
+                Save
+              </button>
+            </div>
           </form>
         </div>
-        <div className="grow px-10 pt-8">
-          <p className="text-2xl font-medium text-[#427A5B] mb-1">Portal Name</p>
+        <div className="grow px-10 py-8 overflow-auto">
+          <p className="text-2xl font-medium text-[#427A5B] mb-1">
+            Portal Name
+          </p>
           <div className="flex flex-row items-center space-x-2">
             <p>May 3 2022</p>
-            <hr style={{
-            color: '#D4D4D4',
-            backgroundColor: '#D4D4D4',
-            height: 2,
-            width: 16
-        }}
-        />
+            <hr
+              style={{
+                color: '#D4D4D4',
+                backgroundColor: '#D4D4D4',
+                height: 2,
+                width: 16,
+              }}
+            />
             <p>May 3 2022</p>
           </div>
-          <p><span className="font-bold pr-2">$12.00</span>fee</p>
-          <p className="leading-5 mt-6 mb-7">Cursus quis quis sit urna, tortor suspendisse vitae mauris. Quis quis volutpat a sit massa et nullam aliquam quam. Viverra faucibus imperdiet consectetur malesuada aliquet. Vitae, urna, imperdiet sem placerat velit convallis suscipit amet est. Consectetur vitae nulla bibendum ornare gravida ultrices. Lacus sit donec egestas donec id. Sit praesent laoreet nulla malesuada accumsan. Consequat sed elit neque, mattis lectus. Amet a fames neque ultricies erat.</p>
-          <hr style={{
-            color: '#D4D4D4',
-            backgroundColor: '#D4D4D4',
-            height: 2,
-            width: 216,
-            margin: 'auto'
-        }}
-        />
-          </div>
+          <p>
+            <span className="font-bold pr-2">$12.00</span>fee
+          </p>
+          <p className="leading-5 mt-6 mb-7">
+            Cursus quis quis sit urna, tortor suspendisse vitae mauris. Quis
+            quis volutpat a sit massa et nullam aliquam quam. Viverra faucibus
+            imperdiet consectetur malesuada aliquet. Vitae, urna, imperdiet sem
+            placerat velit convallis suscipit amet est. Consectetur vitae nulla
+            bibendum ornare gravida ultrices. Lacus sit donec egestas donec id.
+            Sit praesent laoreet nulla malesuada accumsan. Consequat sed elit
+            neque, mattis lectus. Amet a fames neque ultricies erat.
+          </p>
+          <hr
+            style={{
+              color: '#D4D4D4',
+              backgroundColor: '#D4D4D4',
+              height: 2,
+              width: 216,
+              margin: 'auto',
+            }}
+          />
+          <form className="flex flex-col space-y-8 mt-7">
+            {formConfig.map((config) => (
+              <FormInputWrapper key={config.type} type={config.type} />
+            ))}
+          </form>
+        </div>
       </div>
     </div>
   );
