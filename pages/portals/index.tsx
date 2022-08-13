1a9866a5f6d17e357562b6028f052cfc27686354
Perry Asibey-Bonsu <40182399+dev-perry@users.noreply.github.com>
2022-08-13 23:08:50 +0000

    API call fires based on Portal context state

--- pages/portals/index.tsx
+++ pages/portals/index.tsx
@@ -1,20 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import PortalCard from '../../components/PortalCard';
-import Portal from '../../models/Portal';
+import { PortalContext } from '../../contexts/Portal';
 
 const Portals: NextPage = () => {
   const router = useRouter();
-  const [portals, setPortals] = useState<Portal[]>([])
+  const {portals, setPortals} = useContext(PortalContext);
+
   useEffect(() => {
     fetch('/api/portals')
       .then(res => res.json())
       .then(data => {
         setPortals(data)
       })
-  },[])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[portals])
   
   return (
     <div className="pt-12 px-12 h-screen">
@@ -23,7 +25,7 @@ const Portals: NextPage = () => {
       </Head>
       <div className="flex flex-row">
       <p className="text-3xl font-default font-semibold">Portals</p>
-      <button className="h-8 ml-9 text-base rounded border px-4 border-[#427A5B] text-[#427A5B]" onClick={() => router.push("/portals/editor")}>Create Portal</button>
+      <button className="h-8 ml-9 text-base rounded border px-4 border-[#427A5B] text-[#427A5B] hover:text-[#ffffff] hover:bg-[#427A5B]" onClick={() => router.push("/portals/editor")}>Create Portal</button>
       </div>
       <div className="grid grid-cols-3 mt-8 gap-y-6">
         {portals.map((portal) => <PortalCard key={portal.id} name={portal.name} desc={portal.desc} id={portal.id}/>)}
