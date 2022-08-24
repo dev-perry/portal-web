edcbc2d8ac939e3bbe2112464d9cbdc09fcfba70
Perry Asibey-Bonsu <40182399+dev-perry@users.noreply.github.com>
2022-08-24 23:02:03 +0000

    Login and signup pages configured

---
+++ pages/auth/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import AuthForm from '../../components/AuthForm';
+
+const Auth: NextPage = () => {
+  return (
+    <div className="bg-[#F2F2F2] h-screen flex flex-col items-center justify-center relative">
+      <Head>
+        <title>Vostome Portal - Authenticate</title>
+      </Head>
+        <div className="w-52 h-20 absolute left-16 top-12">
+        <img src="/portal-logo.png" alt="Vostome Portal Logo" />
+        </div>         
+     <AuthForm mode='login'/>
+    </div>
+  );
+};
+
+export default Auth;
