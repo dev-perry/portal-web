0af285ef9807860be5678594169c7ad449d255c4
Perry Asibey-Bonsu <40182399+dev-perry@users.noreply.github.com>
2022-08-26 22:19:32 +0000

    Fetch function added to SubmissionViewing

--- contexts/SubmissionViewing.tsx
+++ contexts/SubmissionViewing.tsx
@@ -1,11 +1,13 @@
 import { createContext, useState } from "react"
 import Submission from "../models/Submission"
+import { supabase } from "../utils/supabaseClient";
 
 type ViewingContext = {
     submissions: Submission[];
     selectedSubmission: Submission | undefined;
     setSubmissions: (submissions: Submission[]) => void;
     setSelectedSubmission: (submission: Submission) => void;
+    fetchSubmissions: () => void;
 }
 
 export const ViewingContext = createContext({} as ViewingContext);
@@ -14,9 +16,22 @@ function Viewing({children} : {children: React.ReactNode}){
     const [submissions, setSubmissions] = useState<Submission[]>([]);
     const [selectedSubmission, setSelectedSubmission] = useState<Submission>();
 
+    const fetchSubmissions = async () => {
+        try {
+          const { data, error } = await supabase
+            .from('submissions')
+            .select('id, portal_id, created_on, fields, portals(name)')
+            .limit(100, { foreignTable: 'portals' });
+          if (error) throw error;
+          setSubmissions(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
 
     return(
-        <ViewingContext.Provider value={{submissions, selectedSubmission, setSubmissions, setSelectedSubmission}}>
+        <ViewingContext.Provider value={{submissions, selectedSubmission, setSubmissions, setSelectedSubmission, fetchSubmissions}}>
             {children}
         </ViewingContext.Provider>
     )
