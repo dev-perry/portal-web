0721aa8a6c5561fd10ae9a2472cc1e6997941458
Perry Asibey-Bonsu <40182399+dev-perry@users.noreply.github.com>
2022-08-27 12:27:50 +0000

    Page titles for success and failure pages

--- pages/submit/[pid]/failure.tsx
+++ pages/submit/[pid]/failure.tsx
@@ -1,11 +1,15 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import Head from "next/head"
 
 const SubmitFailure: NextPage = () => {
     const router = useRouter();
     let { pid } = router.query;
     return (
     <div className="m-auto w-full flex flex-col h-full text-white bg-[#9D2F2F] items-center">
+        <Head>
+            <title>Submit Failure</title>
+        </Head>
         <div className="flex flex-col my-auto space-y-6">
         <p className="text-2xl font-medium text-center">Something Broke!</p>
         <button className="h-8 text-base rounded border px-4 border-white text-[#9D2F2F] bg-white" onClick={() => router.push(`/submit/${pid}`)}>Try Again</button>
--- pages/submit/[pid]/success.tsx
+++ pages/submit/[pid]/success.tsx
@@ -1,11 +1,15 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import Head from "next/head"
 
 const SubmitSuccess: NextPage = () => {
     const router = useRouter();
     let { pid } = router.query;
     return (
     <div className="m-auto w-full flex flex-col h-full text-white bg-[#427A5B] items-center">
+        <Head>
+            <title>Submit Success</title>
+        </Head>
         <div className="flex flex-col my-auto space-y-6">
         <p className="text-2xl font-medium text-center">Submission Sent!</p>
         <button className="h-8 text-base rounded border px-4 border-white text-[#427A5B] bg-white" onClick={() => router.push(`/submit/${pid}`)}>Submit Again</button>
